41533bf08c8234b41e324ea47eca59699aca20a5
Ajay Kumar Dewangan <dewanganajay05@gmail.com>
2024-08-31 00:25:26 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+
 import 'package:flutter/material.dart';
-import 'views/onboarding_screen.dart';
-import 'viewmodels/onboarding_viewmodel.dart';
-import 'views/home_screen.dart';
-import 'viewmodels/navigation_viewmodel.dart';
+import 'views/sign_in_screen.dart';
+import 'viewmodels/auth_viewmodel.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,39 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Onboarding App',
+      title: 'Women Safety',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepOrange,
       ),
-      // Using a conditional to check if onboarding is needed
-      home: OnboardingWrapper(),
+      home: SignUpScreen(viewModel: AuthViewModel()),
     );
-  }
-}
-
-class OnboardingWrapper extends StatefulWidget {
-  @override
-  _OnboardingWrapperState createState() => _OnboardingWrapperState();
-}
-
-class _OnboardingWrapperState extends State<OnboardingWrapper> {
-  bool _isOnboardingComplete = false;
-
-  void _completeOnboarding() {
-    setState(() {
-      _isOnboardingComplete = true;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isOnboardingComplete) {
-      return HomeScreen(viewModel: NavigationViewModel());
-    } else {
-      return OnboardingScreen(
-        viewModel: OnboardingViewModel(),
-        onComplete: _completeOnboarding,
-      );
-    }
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,24 +1,329 @@
 import 'package:flutter/material.dart';
-import '../viewmodels/navigation_viewmodel.dart';
-import 'custom_navigation_bar.dart';
+import 'HomeViewModel.dart';
 
 class HomeScreen extends StatelessWidget {
-  final NavigationViewModel viewModel;
+  final HomeViewModel viewModel = HomeViewModel();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  HomeScreen({required this.viewModel});
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Custom Navigation Bar MVVM"),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+        title: const Row(
+          children: [
+            Text(
+              'HELLO, ',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'NAINA',
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(width: 10),
+            Icon(Icons.waving_hand, color: Colors.white, size: 28),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.grey[800],
+              child: Icon(Icons.person, color: Colors.white),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text("Main Content"),
+      drawer: buildDrawer(context),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buildCategoryItem(context, 'Detector', isSelected: true),
+                  buildCategoryItem(context, 'Location'),
+                  buildCategoryItem(context, 'Live Tracking'),
+                ],
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView(
+                  children: [
+                    buildFeatureCardScroll(),
+                    SizedBox(height: 20),
+                    buildFavoritesSection(context),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CustomNavigationBar(viewModel: viewModel),
+      bottomNavigationBar: SafeArea(
+        child: buildBottomNavigationBar(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: FloatingActionButton(
+          backgroundColor: Colors.redAccent,
+          child: Text('SOS', style: TextStyle(fontWeight: FontWeight.bold)),
+          onPressed: () {
+            // Handle SOS button press
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildDrawer(BuildContext context) {
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, Colors.grey[900]!],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.redAccent, Colors.black],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/pattern.png'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'SAAHASINI',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.grey[800],
+                    child: Icon(Icons.person, color: Colors.white, size: 50),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Your Profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            buildDrawerItem(Icons.edit, "Edit Profile"),
+            buildDrawerItem(Icons.lock, "SOS Enable"),
+            buildDrawerItem(Icons.vpn_key, "Change Password"),
+            buildDrawerItem(Icons.notifications, "Notifications"),
+            buildDrawerItem(Icons.settings, "Profile Settings"),
+            buildDrawerItem(Icons.logout, "Logout"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDrawerItem(IconData icon, String title) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.redAccent),
+      title: Text(title, style: TextStyle(color: Colors.white)),
+      onTap: () {
+        // Handle drawer item tap
+      },
+    );
+  }
+
+  Widget buildCategoryItem(BuildContext context, String title, {bool isSelected = false}) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            color: isSelected ? Colors.redAccent : Colors.white,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+        if (isSelected)
+          Container(
+            margin: EdgeInsets.only(top: 4),
+            height: 2,
+            width: 30,
+            color: Colors.redAccent,
+          )
+      ],
+    );
+  }
+
+  Widget buildFeatureCardScroll() {
+    return SizedBox(
+      height: 180,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          buildFeatureCard('Spy Camera Detector', 'assets/images/camera_icon.png', Colors.deepPurpleAccent),
+          SizedBox(width: 10),
+          buildFeatureCard('Emergency Contact', 'assets/images/emergency_icon.png', Colors.blueAccent),
+          SizedBox(width: 10),
+          buildFeatureCard('Live Tracking', 'assets/images/tracking_icon.png', Colors.orangeAccent),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFeatureCard(String title, String imagePath, Color color) {
+    return Container(
+      width: 150,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color.withOpacity(0.8), color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: color.withOpacity(0.5),
+            offset: Offset(0, 4),
+            blurRadius: 10,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(imagePath, height: 80, color: Colors.white),
+          SizedBox(height: 10),
+          Text(title, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFavoritesSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Favourites',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // Handle "See all" press
+              },
+              child: Text('See all', style: TextStyle(color: Colors.blue)),
+            ),
+          ],
+        ),
+        ListTile(
+          leading: Icon(Icons.feedback, color: Colors.redAccent),
+          title: Text('Education Feedback', style: TextStyle(color: Colors.white)),
+        ),
+        ListTile(
+          leading: Icon(Icons.feedback, color: Colors.redAccent),
+          title: Text('Code Generation', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+
+  Widget buildBottomNavigationBar() {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 10,
+      child: Container(
+        height: 70,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              offset: Offset(0, -1),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.white),
+              onPressed: () {
+                // Navigate to Home
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.list, color: Colors.white),
+              onPressed: () {
+                // Navigate to Articles
+              },
+            ),
+            SizedBox(width: 40), // Space for the FAB
+            IconButton(
+              icon: Icon(Icons.settings, color: Colors.white),
+              onPressed: () {
+                // Navigate to Settings
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.person, color: Colors.white),
+              onPressed: () {
+                // Navigate to Profile
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
